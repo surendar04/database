defaa2a1b12f5eaa9d25753358a2ab5d0f0fb38d
varun dev <mk.varun2020@gmail.com>
2022-07-24 17:42:14 +0530

    variablebase

---
+++ variableBasis.js
@@ -0,0 +1,27 @@
+
+console.log("using double quotes to represent the string")
+console.log('using single quotes to represent the string')
+console.log("no need of double or single quotes to represent the integer and float")
+console.log(423)
+
+// "this represent the variable assingment to declare"
+let name ="surendar";
+console.log(name);
+name="john wick"
+console.log(name)
+const phoneNumber=42342
+console.log(phoneNumber)
+//phoneNumber=34234 we cant change the value
+//console.log(phoneNumber)
+
+//finding the type of the variable by using typeof function
+
+//this typeof function is used to indicate the variable type
+console.log(typeof(null))
+
+//introduced to minimize the complexity ${}
+console.log(`  i am ${name} my numer  ${phoneNumber}` )
+console.clear()
+//undefine which is used to variable which is not assigned the value
+let unknownPerson
+console.log(unknownPerson)
