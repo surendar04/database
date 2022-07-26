042b9dce51d710619358bf612bc20c8dc756605c
varun dev <mk.varun2020@gmail.com>
2022-07-26 20:46:29 +0530

    learnt stringsmethod and operators

---
+++ operator.js
@@ -0,0 +1,27 @@
+let surendarPoints=0;
+let kathirPoints=0;
+
+surendarPoints=surendarPoints+10
+kathirPoints=surendarPoints+10
+kathirPoints=kathirPoints-5
+surendarPoints++
+--kathirPoints
+surendarPoints=surendarPoints*2
+kathirPoints=kathirPoints**2
+
+kathirPoints=kathirPoints/2
+//divided by 0 will be introduced to the infinity 
+//kathirPoints=kathirPoints/0
+kathirPoints=kathirPoints%2
+ 
+
+
+
+console.log(`surendar\tPoints: ${surendarPoints}`)
+console.log(`kathir \t\tPoints: ${kathirPoints}`)
+
+const collegefee=4543
+const dicountpercent=10
+const dicountamount=dicountpercent/100*collegefee
+
+console.log(`final fees${dicountamount}`)
---
+++ stringMethods.js
@@ -0,0 +1,18 @@
+let person="   surendar is good boy"
+//to find the length of the  string
+console.log(`Length : ${person.length}`)
+
+console.log(`Uppercase : ${person.toUpperCase()}`)
+
+console.log(`Lowercase : ${person.toLowerCase()}`)
+
+console.log(`Replace : ${person.replace("good","bad")}`)
+
+console.log(`CharacterAt: ${person.charAt(5)}`)
+
+console.log(`Concate :surendar ${person.concat(" but a bad boys")}`)
+//trim is used to reduce the empty space
+console.log(`Trim : ${person.trim()}`)
+//used to print the value assigned to it from this value to value
+ console.log(`slice : ${person.slice(5,7)}`)
+
